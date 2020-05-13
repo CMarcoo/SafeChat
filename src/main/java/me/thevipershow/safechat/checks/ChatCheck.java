ef0c9aefc9e85c17bdf4c6e3757e99205f4e25cc
TheViperShow <29604693+TheViperShow@users.noreply.github.com>
2020-05-13 18:41:18 +0200

    Update ChatCheck.java

--- src/main/java/me/thevipershow/safechat/checks/ChatCheck.java
+++ src/main/java/me/thevipershow/safechat/checks/ChatCheck.java
@@ -26,6 +26,7 @@ package me.thevipershow.safechat.checks;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.Plugin;
 
+@FunctionalInterface
 public interface ChatCheck {
     void result(final String message, final AsyncPlayerChatEvent chatEvent, final Plugin plugin);
 }
