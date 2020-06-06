c9d98140961be7b30b80fd303f8dba8619fd1060
TheVIperShow <thevipershowita@gmail.com>
2020-06-07 00:34:01 +0200

    Starting to add "search" command

--- src/main/java/me/thevipershow/safechat/common/commands/SafeChatCommand.java
+++ src/main/java/me/thevipershow/safechat/common/commands/SafeChatCommand.java
@@ -19,13 +19,16 @@
 package me.thevipershow.safechat.common.commands;
 
 import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.Description;
-import co.aikar.commands.annotation.Subcommand;
+import co.aikar.commands.annotation.*;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import me.thevipershow.safechat.common.configuration.AbstractValues;
+import me.thevipershow.safechat.common.sql.DataManager;
+import me.thevipershow.safechat.common.sql.data.Flag;
+import me.thevipershow.safechat.common.sql.data.PlayerData;
 import net.kyori.text.adapter.bukkit.TextAdapter;
 import net.kyori.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.command.CommandSender;
@@ -35,11 +38,12 @@ import org.bukkit.command.CommandSender;
 @Description("&7Main command of the SafeChat plugin")
 public final class SafeChatCommand extends BaseCommand {
     private final AbstractValues values;
+    private final DataManager dataManager;
     private static SafeChatCommand instance = null;
     private final static String PREFIX = "&8[&6SafeChat&8]&f: ";
 
-    public static synchronized SafeChatCommand getInstance(AbstractValues values) {
-        return instance != null ? instance : (instance = new SafeChatCommand(values));
+    public static synchronized SafeChatCommand getInstance(AbstractValues values, DataManager dataManager) {
+        return instance != null ? instance : (instance = new SafeChatCommand(values, dataManager));
     }
 
     /**
@@ -84,6 +88,30 @@ public final class SafeChatCommand extends BaseCommand {
         } catch (IllegalArgumentException e) {
             sendMessage(sender, PREFIX + "&7Configuration could not be updated correctly, issue below:");
             sendMessage(sender, PREFIX + "&c" + e.getMessage());
+        }
+    }
+
+    @Subcommand("search")
+    @Syntax("&8<&6player&8> &8[&6ipv4&7|&6domains&7|&6words&8] &7- Search data of a player.")
+    @CommandPermission("safechat.commands.reload")
+    @CommandCompletion("@checks")
+    public void onSearch(CommandSender sender, String name, @Optional String flag) {
+        if (flag == null) {
+
+        } else {
+            try {
+                Flag f = Flag.valueOf(flag);
+                Set<PlayerData> set = dataManager.getPlayerData().parallelStream()
+                        .filter(data -> data.getUsername().equals(sender.getName()))
+                        .collect(Collectors.toSet());
+                if (!set.isEmpty()) {
+                    set.forEach(data -> sendMessage(sender, PREFIX + "&7Player &6" + name + "&7has &6" + data.getFlags().get(f) + " &7flags"));
+                } else {
+                    sendMessage(sender, PREFIX + "&7No data was found for player &6" + name);
+                }
+            } catch (IllegalArgumentException e) {
+                sendMessage(sender, PREFIX + "&7Invalid flag type.");
+            }
         }
     }
 }
