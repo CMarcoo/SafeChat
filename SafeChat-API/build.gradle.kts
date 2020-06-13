b01ce0b14b8049035f33dc3704d901f367f24d07
TheVIperShow <thevipershowita@gmail.com>
2020-06-13 19:01:49 +0200

    Fixed modules issues.

--- SafeChat-API/build.gradle.kts
+++ SafeChat-API/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    id("com.github.johnrengelman.shadow") version "5.2.0"
     id("io.freefair.lombok") version "5.1.0"
 }
 
@@ -7,6 +8,7 @@ group = "me.thevipershow"
 version = "1.1.1"
 
 repositories {
+    jcenter()
     mavenCentral()
     maven(url = "https://jitpack.io")
     maven(url = "https://repo.aikar.co/content/groups/aikar/")
@@ -18,6 +20,12 @@ dependencies {
     compileOnly(project(":SafeChat-Plugin"))
     implementation(project(":SafeChat-Core"))
     compileOnly(group = "com.destroystokyo.paper", name = "paper-api", version = "1.15.2-R0.1-SNAPSHOT")
+}
+
+tasks {
+    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+
+    }
 }
 
 configure<JavaPluginConvention> {
